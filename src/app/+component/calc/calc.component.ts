3c89799c2b62a47d933bd7337a2819271ff3c41b
Amir <amirsiahvashi25@gmail.com>
2024-07-04 23:59:46 +0330

    v4

--- src/app/+component/calc/calc.component.ts
+++ src/app/+component/calc/calc.component.ts
@@ -8,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrl: './calc.component.css'
 })
 export class CalcComponent {
-
+  num1: string = '0';
+  num2: string = '0';
+  operator: string = '-';
+  click(c: string) {
+    this.operator = c;
+    this.num1 = this.num2;
+    this.num2 = '0';
+  }
+  digit(d: string) {
+    this.num2 = Number(this.num2 + d).toString();
+  }
+  op() {
+    if (this.operator === '+') {
+      this.num2 = (Number(this.num1) + Number(this.num2)).toString();
+    } else if (this.operator === '-') {
+      this.num2 = (Number(this.num1) - Number(this.num2)).toString();
+    } else if (this.operator === 'x') {
+      this.num2 = (Number(this.num1) * Number(this.num2)).toString();
+    } else if (this.operator === '÷') {
+      this.num2 = (Number(this.num1) / Number(this.num2)).toString();
+    }
+    this.operator = '-';
+    this.num1 = '0';
+  }
 }
